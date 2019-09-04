7054deb9733923c740eb8d18483ca1e83b752e79
unknown <[shrestha_sl@hotmail.com]>
2019-09-03 21:01:54 -0500

    Api layer added

--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,16 +1,62 @@
-let journalEntry = {
-   
+
+let entriesAPI_URL = "http://localhost:3000/entries"
+
+function createJournalHTML(journalObj) {
+    return `
+    <section class="journal_entry">
+         <h2>${journalObj.concept}</h2>
+         <p>${journalObj.entry}</p>
+         <p>${journalObj.mood}</p>
+         <p>${journalObj.date}</p>
+    </section>
+    `
 }
 
-let journalEntries = [];
+document.querySelector("#submit").addEventListener("click", (event) => {
+    const journalDate = document.querySelector("#journalDate").value
+    const concept = document.querySelector("#conceptCovered").value
+    const entry = document.querySelector("#journalEntry").value
+    const mood = document.querySelector("#moodOfTheDay").value
 
+    const dailyJournal = {
+        date: journalDate,
+        concept: concept,
+        entry: entry,
+        mood: mood
+    }
+   fetch(entriesAPI_URL, {
+      method: 'POST',
+      body: JSON.stringify(dailyJournal),
+      headers: {
+          'Content-Type': 'application/json'
+      } 
+   }).then (res => res.json()) 
+     .then(response => console.log('Success:', JSON.stringify(response)))
+     .catch(error => console.log('Error:', error));
+     loadEntries();
+    
+});
+ const loadEntries = () => {
+    fetch(entriesAPI_URL)
+    .then(data => data.json())
+    .then(parsedData => {
+        renderJournalEntries(parsedData);
+    })
+ }
 
-function addJournal(){
-    journalEntry.journalDate = document.getElementById("journalDate").value;
-    journalEntry.conceptCovered = document.getElementById("conceptCovered").value;
-    journalEntry.journalEntry = document.getElementById("journalEntry").value;
-    journalEntry.moodOfTheDay = document.getElementById("moodOfTheDay").value;
-
-    journalEntries.push(journalEntry);
-    console.log(journalEntries)
+const renderJournalEntries = (entries) => {
+         entries.forEach(item => {
+         let dailyJournal = createJournalHTML(item)
+         document.querySelector(".journalList").innerHTML += dailyJournal
+    
+    })
 }
+
+loadEntries();
+
+
+
+
+
+
+
